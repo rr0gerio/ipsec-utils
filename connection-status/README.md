d3a144e0173825b16f98858e4e6f74e946009783
rr0gerio <rogeriosoares01@hotmail.com>
2021-10-22 12:08:52 -0300

    connection-status

---
+++ connection-status/README.md
@@ -0,0 +1,15 @@
+# IPSEC Status
+
+## Requirements
+
+- Python 3.7
+- Linux Libreswan 3.25 or similar
+
+
+This script runs above `ipsec auto --status` command, organizing and showing each connection status.
+
+## Instalation:
+
+Just copy the status.py file to the server and run. Ex:
+
+`python3.7 status.py`
---
+++ connection-status/status.py
@@ -0,0 +1,38 @@
+import os
+
+RED = '31m'
+GREEN = '32m'
+
+def colorize(color, text):
+    colored_text = f"\033[{color}{text}\033[00m"
+    return colored_text
+
+def getConnections():
+    conns = os.popen("ipsec auto --status | grep '==='| awk -F ' ' '{print $2}' | awk -F '/' '{print $1}' | sed -E -e 's/\"|://g' | uniq | sort").read().split("\n")
+    del conns[-1]
+    return conns
+
+def verifyConnections(conns):
+    
+    for conn in conns:
+        publicIP = os.popen("ipsec auto --status | grep -w \\\""+conn+"\\\": | grep 'their id=' | awk -F ';' '{print $4}'| uniq | awk -F'=' '{print $2}'").read()
+        try:
+            tunnels = os.popen("ipsec auto --status | grep -w \\\""+conn+"\\\": | grep ===")
+            print(f"\nConexão: {conn.upper()} - {publicIP}")
+            for tunnel in tunnels:
+                host = tunnel.split("===")[2].split(";")[0]
+                status = tunnel.split("===")[2].split(";")[1]
+                con_type = 'Subnet' if host.find("/32") != 1 else 'Host'
+                if status == " erouted":
+                    msg = colorize(GREEN,'OK!')
+                else:
+                    msg = colorize(RED, 'Verifique a conexão com o cliente') 
+                print(f"    {con_type}: {host} {msg}")
+
+        except Exception as e:
+            print ("Error occured: {}".format(e))
+
+
+if __name__ == "__main__":
+    conns = getConnections()
+    verifyConnections(conns)
